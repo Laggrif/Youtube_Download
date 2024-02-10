8e077b08c6350f58fa6fd1b0dfe12ceb9ebee926
Laggrif <ugo.novello@epfl.ch>
2024-02-10 16:58:07 +0100

    Changed installer from pyinstall+installForge to pynsist

--- src/Directory.py
+++ src/Directory.py
@@ -1,6 +1,6 @@
 import os
 import sys
 
-application_path = "."
-if getattr(sys, 'frozen', False):
-    application_path = os.path.dirname(sys.executable) + "\\_internal"
+application_path = os.path.dirname(sys.modules['__main__'].__file__)
+
+data_path = os.path.join(os.environ['APPDATA'], 'Youtube Downloader')
