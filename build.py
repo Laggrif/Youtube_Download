360be1f16641f9ffec1b1aaab7387e33526f9668
Laggrif <ugo.novello@epfl.ch>
2024-02-10 16:57:27 +0100

    Changed installer from pyinstall+installForge to pynsist

--- build.py
+++ build.py
@@ -1,27 +1,12 @@
 import os
 import shutil
+import winsound
 
 if __name__ == "__main__":
 
     os.chdir(os.path.dirname(__file__))
+    if os.path.exists('./src/res/config'):
+        shutil.rmtree('./src/res/config')
 
-    os.system('.\\venv\\Scripts\\pyinstaller '
-              '--noconfirm '
-              '--onedir '
-              '--windowed '
-              '--icon "C:/Users/Laggrif/Documents/Coding_Projects/Youtube Download/src/res/icon/download.ico" '
-              '--name "Youtube Download" '
-              '--clean '
-              '--add-data "C:/Users/Laggrif/Documents/Coding_Projects/Youtube Download/src/config.py;." '
-              '--add-data "C:/Users/Laggrif/Documents/Coding_Projects/Youtube Download/src/Directory.py;." '
-              '--add-data "C:/Users/Laggrif/Documents/Coding_Projects/Youtube Download/src/MainWindow.py;." '
-              '--add-data "C:/Users/Laggrif/Documents/Coding_Projects/Youtube Download/src/res/style.qss;res/" '
-              '--add-data "C:/Users/Laggrif/Documents/Coding_Projects/Youtube Download/src/utils.py;." '
-              '--add-data "C:/Users/Laggrif/Documents/Coding_Projects/Youtube Download/src/Youtube_music_downloader.py;." '
-              '--add-data "C:/Users/Laggrif/Documents/Coding_Projects/Youtube Download/src/YTDL.py;." '
-              '--add-data "C:/Users/Laggrif/Documents/Coding_Projects/Youtube Download/src/ffmpeg;ffmpeg/" '
-              '--add-data "C:/Users/Laggrif/Documents/Coding_Projects/Youtube Download/src/res/icon;res/icon" '
-              '"C:/Users/Laggrif/Documents/Coding_Projects/Youtube Download/src/Run.py"')
-    if os.path.exists('./dist/Youtube Download/_internal/res/config'):
-        shutil.rmtree('./dist/Youtube Download/_internal/res/config')
-    shutil.copy('src/vc.exe', './dist/Youtube Download')
+    os.system('pynsist .\\installer.cfg')
+    winsound.PlaySound('SystemExclamation', winsound.SND_ALIAS)
