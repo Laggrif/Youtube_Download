c8ee63f7c3e5d4f32c0969ed8781ef6a84d37b7f
Laggrif <ugo.novello@epfl.ch>
2025-08-18 12:09:33 +0200

    Now uses regexp

--- src/YTDL/logic/string_parser.py
+++ src/YTDL/logic/string_parser.py
@@ -1,10 +1,13 @@
+import re
+
 
 class Parser:
     def __init__(self, infos):
         self.infos = infos
 
-    def parse(self, string):
+    def parse(self, string: str) -> str:
         for key, value in self.infos.items():
             if key in string and isinstance(value, str):
                 string = string.replace('{' + key + '}', value)
+        string = re.sub(r'\{.*?\}', '', string)
         return string
